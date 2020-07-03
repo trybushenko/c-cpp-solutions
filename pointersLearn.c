8ac94e9471c88b3cbfadbd1e29819a54ed604e03
Artem Trybushenko <tribushenko02@gmail.com>
2020-07-03 21:30:26 +0300

    Learned what pointers are and slightly learned how to work with them

---
+++ pointersLearn.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#define TAX 23
+
+/*
+Here I am learning  what pointers are and how to work with them
+*/
+void  main (void) {
+    // declaring and initializing variables
+  char one = 1;
+  char two = 2;
+  char three = 3;
+  char four = 4;
+  char five = 5;  
+  // declaring and initializing ponters to variables
+  char* onePointer = NULL;
+  char* twoPointer = NULL;
+  char* threePointer = NULL;
+  char* fourPointer = NULL;
+  char* fivePointer = NULL;
+  //assigning adresses of 1-5 variables to pointers declaread and initialized above
+  onePointer = &one;
+  twoPointer = &two;
+  threePointer = &three;
+  fourPointer = &four;
+  fivePointer = &five;
+  //declaring and initializing variables to store data of dereferenced pointers
+  char oneResult = 0;
+  char twoResult = 0;
+  char threeResult = 0;
+  char fourResult = 0;
+  char fiveResult = 0;
+    //dereferencing pointers and assigning their values to initialized variables above
+  oneResult = *onePointer;
+  twoResult = *twoPointer;
+  threeResult = *threePointer;
+  fourResult = *fourPointer;
+  fiveResult = *fivePointer;
+  //set new values to value of pointers variables
+  *onePointer = 10;
+  *twoPointer = 20;
+  *threePointer = 30;
+  *fourPointer = 40;
+  *fivePointer = 50;
+  //getting to know the result variables(according to logic they seem to be changed because of altering value of pointers)
+  printf(" *onePointer = %i\r\n *twoPointer = %i\r\n *threePointer = %i\r\n *fourPointer = %i\r\n *fivePointer = %i\r\n", one, two, three, four, five);
+}
